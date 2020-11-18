8726381e167c9e8db64d6ea54c088ab3ca55b367
Rohan Bhandari <rohan.bhandari@students.iiit.ac.in>
2020-11-18 16:30:24 +0530

    Web app made test it

---
+++ ocr-webapp/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+import requests
+import json
+import os
+from werkzeug.utils import secure_filename
+import cv2 
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/'
+file_process = ''
+
+def ocr_vehicle(filename):
+    url = "https://cloud.eyedea.cz/api/v2/cardetect.json"
+
+    payload = {'email': 'rohan27bhandari@outlook.com',
+    'password': '1199efa85',
+    'Content-type': 'image/jpeg'}
+    files = [
+    ('upload', open(filename, 'rb'))
+    ]
+    headers= {}
+
+    response = requests.request("POST", url, headers=headers, data = payload, files = files)
+    json_data =  json.loads(response.text.encode('utf8'))
+    message = None
+    try:
+        message = 'License Plate(s) detected: '
+        for car in json_data['photos'][0]['tags']:
+            message += car['lp_text_content'] + ', '
+        message = message[:-2]
+    except:
+        message = ''
+    return message
+
+def process_frames(filename):
+    try: 
+        cam = cv2.VideoCapture(filename)
+        if not os.path.exists('data'): 
+            os.makedirs('data') 
+    except OSError: 
+        print ('Error: Creating directory of data')
+    currentframe = 0
+    while(True):
+        ret, frame = cam.read()
+        if ret: 
+            name = 'frame' + str(currentframe) + '.jpg'
+            print ('Creating...' + name) 
+            cv2.imwrite(name, frame)
+            message = ocr_vehicle(name)
+            os.remove(name)
+            if len(message) > 0:
+                return message
+            currentframe += 1
+        else: 
+            break 
+    cam.release() 
+    cv2.destroyAllWindows()
+    return ''
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    global file_process
+    if request.method == 'POST':
+      f = request.files['file']
+      filename = app.config['UPLOAD_FOLDER'] + secure_filename(f.filename)
+      f.save(filename)
+      file_process = filename
+      return redirect(url_for('uploaded'))
+    return render_template('index.html')
+
+@app.route('/uploaded', methods=['GET','POST'])
+def uploaded():
+    global file_process
+    alert_message = process_frames(file_process)
+    if alert_message != '':
+        return render_template('uploaded.html', msg=alert_message, video=file_process)
+    return render_template('uploaded.html', video=file_process)
+
+if __name__ == "__main__":
+    app.run(debug=True)    
